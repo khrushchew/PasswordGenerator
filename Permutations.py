97e9360165da6cdc0f9b06bcc87ad50c3d20160a
Никита <popovn2268@gmail.com>
2024-01-28 15:08:42 +0300

    Clear

--- Permutations.py
+++ Permutations.py
@@ -1,20 +1,28 @@
 from passKeyСipher import charToNum
 from math import fabs
 from classesOfExceptions import PasswordError
+from random import choice
+
 
 class Password:
     @staticmethod
     def matrix(charlist: str) -> str:
-        if len(charlist) == 9:
+        if len(charlist) == 9 and charlist.isalpha():
             det_matrix = (charToNum[charlist[0]] * charToNum[charlist[4]] * charToNum[charlist[8]] +
-                          charToNum[charlist[3]] * charToNum[charlist[7]] * charToNum[charlist[2]] + charToNum[charlist[1]]
+                          charToNum[charlist[3]] * charToNum[charlist[7]] * charToNum[charlist[2]] + charToNum[
+                              charlist[1]]
                           * charToNum[charlist[5]] * charToNum[charlist[6]] - charToNum[charlist[6]] *
-                          charToNum[charlist[4]] * charToNum[charlist[2]] - charToNum[charlist[0]] * charToNum[charlist[7]]
+                          charToNum[charlist[4]] * charToNum[charlist[2]] - charToNum[charlist[0]] * charToNum[
+                              charlist[7]]
                           * charToNum[charlist[5]] - charToNum[charlist[1]] * charToNum[charlist[3]] *
                           charToNum[charlist[8]])
             return str(fabs(det_matrix))[0]
         else:
-            raise PasswordError("Too few characters")
+            raise PasswordError("Invalid characters")
+
     @staticmethod
-    def srt(nums: int) -> str:
-        pass
+    def actor(name: str) -> str:
+        if name.isalpha():
+            return choice(list(name)).upper()
+        else:
+            raise PasswordError("It is not name)")
--- appWindow.py
+++ appWindow.py
@@ -9,6 +9,7 @@ window = Tk()
 counter = 0
 mass = []
 
+
 def loop():
     global counter
     try:
@@ -17,12 +18,19 @@ def loop():
             label_for_result['text'] = 'OK!'
             label_for_result.pack()
             counter += 1
-        if counter == 1:
             entry_for_password.delete(first=0, last=END)
             label_for_password['text'] = 'Now write the name of your favorite actor'
+        elif counter == 1:
+            mass.append(Password.actor(entry_for_password.get()))
+            label_for_result['text'] = 'OK!'
+            label_for_result.pack()
+            counter += 1
+            entry_for_password.delete(first=0, last=END)
+            label_for_password['text'] = ''
     except PasswordError as e:
         label_for_result['text'] = f'User Error!\n{e}'
         label_for_result.pack()
+
 
 window.title('Password Generator')
 window.iconbitmap(default='D:\\PyProjects\\GitHub\\PasswordGenerator\\Icons\\win.ico')
