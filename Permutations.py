d02ba32a9f4b8b05739f6b074b86c5124e17459e
Никита <popovn2268@gmail.com>
2024-01-28 14:36:24 +0300

    Clear

--- Permutations.py
+++ Permutations.py
@@ -1,18 +1,20 @@
 from passKeyСipher import charToNum
 from math import fabs
-
+from classesOfExceptions import PasswordError
 
 class Password:
     @staticmethod
     def matrix(charlist: str) -> str:
-        det_matrix = (charToNum[charlist[0]] * charToNum[charlist[4]] * charToNum[charlist[8]] +
-                      charToNum[charlist[3]] * charToNum[charlist[7]] * charToNum[charlist[2]] + charToNum[charlist[1]]
-                      * charToNum[charlist[5]] * charToNum[charlist[6]] - charToNum[charlist[6]] *
-                      charToNum[charlist[4]] * charToNum[charlist[2]] - charToNum[charlist[0]] * charToNum[charlist[7]]
-                      * charToNum[charlist[5]] - charToNum[charlist[1]] * charToNum[charlist[3]] *
-                      charToNum[charlist[8]])
-        return str(fabs(det_matrix))[0]
-
+        if len(charlist) == 9:
+            det_matrix = (charToNum[charlist[0]] * charToNum[charlist[4]] * charToNum[charlist[8]] +
+                          charToNum[charlist[3]] * charToNum[charlist[7]] * charToNum[charlist[2]] + charToNum[charlist[1]]
+                          * charToNum[charlist[5]] * charToNum[charlist[6]] - charToNum[charlist[6]] *
+                          charToNum[charlist[4]] * charToNum[charlist[2]] - charToNum[charlist[0]] * charToNum[charlist[7]]
+                          * charToNum[charlist[5]] - charToNum[charlist[1]] * charToNum[charlist[3]] *
+                          charToNum[charlist[8]])
+            return str(fabs(det_matrix))[0]
+        else:
+            raise PasswordError("Too few characters")
     @staticmethod
     def srt(nums: int) -> str:
         pass
--- appWindow.py
+++ appWindow.py
@@ -1,50 +1,56 @@
 import tkinter
 
-import logic
+from classesOfExceptions import PasswordError
+from Permutations import Password
 from tkinter import *
 
 window = Tk()
 
+counter = 0
+mass = []
+
+def loop():
+    global counter
+    try:
+        if counter == 0:
+            mass.append(Password.matrix(entry_for_password.get()))
+            label_for_result['text'] = 'OK!'
+            label_for_result.pack()
+            counter += 1
+        if counter == 1:
+            entry_for_password.delete(first=0, last=END)
+            label_for_password['text'] = 'Now write the name of your favorite actor'
+    except PasswordError as e:
+        label_for_result['text'] = f'User Error!\n{e}'
+        label_for_result.pack()
+
 window.title('Password Generator')
-window.iconbitmap(default='D:\PyProjects\GitHub\PasswordGenerator\Icons\win.ico')
+window.iconbitmap(default='D:\\PyProjects\\GitHub\\PasswordGenerator\\Icons\\win.ico')
 
 window.geometry('800x600+800+400')
 window.resizable(False, False)
 window.configure(bg='#B0C4DE')
 
 label_of_hello = tkinter.Label(text='Hello!', anchor='center', font=('Consolas', 24), background='#B0C4DE', pady=20)
-label_of_main = tkinter.Label(text='This program is designed to generate\n a password according to a given algorithm', anchor='center', font=('Consolas', 24),
+label_of_main = tkinter.Label(text='This program is designed to generate\n a password according to a given algorithm',
+                              anchor='center', font=('Consolas', 24),
                               background='#B0C4DE', pady=20)
 label_of_hello.pack()
 label_of_main.pack()
 
-label_for_password = tkinter.Label(text='First, write 9 characters\n from the English alphabet', anchor='center', font=('Consolas', 24),
+label_for_password = tkinter.Label(text='First, write 9 characters\n from the English alphabet', anchor='center',
+                                   font=('Consolas', 24),
                                    background='#B0C4DE', pady=20)
 label_for_password.pack()
 
 entry_for_password = tkinter.Entry(font=('Consolas', 24))
 entry_for_password.pack()
 
-button_for_click = tkinter.Button(text="Click", font=('Consolas', 24), command=get_text)
-c = entry_for_password.get()
-
-# def get_text():
-#     global counter
-#     mass.append(entry_for_password.get())
-#     if counter == 0:
-#         label_for_password['text'] = 'fwefwefwefwef'
-#         label_for_result['text'] = Permutations.Password.matrix(mass[self.__counter])
-#         counter += 1
-#     elif counter == 1:
-#         label_for_password['text'] = 'fwefw'
-#         counter += 1
-#
-
+button_for_click = tkinter.Button(text='Click', font=('Consolas', 24), command=loop)
 button_for_click.pack(anchor='center', pady=20)
 
-label_for_result = tkinter.Label(text='fewf', anchor='center', font=('Consolas', 24),
-                                   background='#B0C4DE', pady=20)
-label_for_result.pack()
+label_for_result = tkinter.Label(text='', anchor='center', font=('Consolas', 24),
+                                 background='#B0C4DE', pady=20)
 
 window.mainloop()
-
+print(mass)
